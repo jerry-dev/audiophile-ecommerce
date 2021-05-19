c6141e9cda5025e4f7cff8f6f54edc95ba2f87d7
JerryD <jerry.webdev@gmail.com>
2021-05-19 05:28:35 -0400

    Commit Path Mapping And ProductCategory Initialization
    Maps the link paths for "/heaphones", "/speakers", and "/earphones"
    within the root AudiophileApp component.
    Initialized the ProductCategory.js file within the newly initialized
    productcategory/src directory.

--- src/components/audiophileapp/AudiophileApp.js
+++ src/components/audiophileapp/AudiophileApp.js
@@ -1,7 +1,9 @@
+import store from '../../lib/store/index.js';
 import AppHeader from '../appheader/AppHeader.js';
 import HomeSection from '../homesection/src/HomeSection.js';
 import AppFooter from '../appfooter/AppFooter.js';
-import store from '../../lib/store/index.js';
+// import ProductCategory from '../productCategory/src/ProductCategory.js';
+
 
 class AudiophileApp extends HTMLElement {
     constructor() {
@@ -14,7 +16,7 @@ class AudiophileApp extends HTMLElement {
         this.routerInit();
         let self = this;
         this.store = store;
-        this.store.observer.subscribe('stateChange', () => { this.pathSync(this) });
+        this.store.observer.subscribe('stateChange', () => { this.pathMap(this) });
     }
 
     render() {
@@ -48,15 +50,39 @@ class AudiophileApp extends HTMLElement {
         this.routerOutput = this.shadowRoot.querySelector("#routerOutput");
         this.router = new Navigo("/");
 
-        this.router.on('/', () => this.routerOutput.innerHTML = `<home-section></home-section>`);
+        this.router.on('/', () => {
+            this.routerOutput.innerHTML = `<home-section></home-section>`
+        });
+
+        this.router.on('/headphones', () => {
+            this.routerOutput.innerHTML = `<product-category></product-category>`
+            // fetch
+        });
+
+        this.router.on('/speakers', () => {
+            this.routerOutput.innerHTML = `<product-category></product-category>`
+            // fetch
+        });
+
+        this.router.on('/earphones', () => {
+            this.routerOutput.innerHTML = `<product-category></product-category>`
+            // fetch
+        });
+
         this.router.on('/test', () => this.routerOutput.innerHTML = `<h1>TEST</h1>`);
         this.router.resolve();
     }
 
-    pathSync(context) {
+    pathMap(context) {
         let path = context.store.state.path;
         switch (path) {
             case '/': context.router.navigate("/");
+                break;
+            case '/headphones': context.router.navigate("/headphones");
+                break;
+            case '/speakers': context.router.navigate("/speakers");
+                break;
+            case '/earphones': context.router.navigate("/earphones");
                 break;
             case '/test': context.router.navigate("/test");
                 break;
